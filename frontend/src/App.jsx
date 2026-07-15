0c6ac23c7bcc9b801157cb41fcae168a00052345
khedev <bualkhedev@gmail.com>
2026-07-15 13:09:13 +0800

    chore: disable react-query-devtools

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import router from '@/routes';
 import useAuthStore from '@/store/authStore';
@@ -52,7 +51,6 @@ function App() {
     >
       <RouterProvider router={router} />
       <Toaster position="top-right" />
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
     </PersistQueryClientProvider>
   );
 }
